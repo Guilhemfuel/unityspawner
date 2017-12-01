56bb83f28f5a0c90ea9ef4f049ade31581b2c928
ECV <ecv@ECV-BO-B018-IM026.local>
2017-12-01 14:50:15 +0100

    First commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject a;
+	public GameObject b;
+	public GameObject c;
+	public GameObject d;
+	public GameObject cube;
+
+	private float timeCounter = 0;
+	public float delay = 0.200f; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCounter += Time.deltaTime;
+	
+
+		if (timeCounter > delay) {
+			Debug.Log (timeCounter);
+
+			timeCounter = 0;
+
+			GameObject currentCube = Instantiate (cube);
+			currentCube.transform.position = a.transform.position;
+		}
+	}
+}
---
+++ Assets/drop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drop : MonoBehaviour {
+
+	public float vitesse = 10;
+	public float posVmax = -200;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = gameObject.transform.position;
+		position.y -= vitesse;
+		gameObject.transform.position = position;
+
+		if (gameObject.transform.position.y <= posVmax) {
+			Destroy(gameObject);
+		}
+	}
+}
